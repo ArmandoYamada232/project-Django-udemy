4220e151d39170078b6e91b0bfced497a1edfe1f
Armando Isamu de Almeida Yamada <armandoyamada23@icloud.com>
2026-08-20 09:56:53 -0300

    Exemplo de Temp

--- recipes/views.py
+++ recipes/views.py
@@ -6,7 +6,9 @@ from django.http import HttpResponse
 
 # tem que dar namespace para arquivos de template
 def home(request):#Essa string é o caminho até o html
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', status= 200, context={
+        'name': 'Armando',
+    })
     #Adiciona a pasta e depois o arquivo que está na pasta
 
 def contato(request):
