1bf0d8a82330338922cc3ab9daf7a385bf34513d
Armando Isamu de Almeida Yamada <armandoyamada23@icloud.com>
2026-08-31 15:09:49 -0300

    migração do 001_initial

--- recipes/migrations/0001_initial.py
+++ recipes/migrations/0001_initial.py
@@ -13,6 +13,7 @@ class Migration(migrations.Migration):
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
+    #Gera automaticamente os campos de ID como primaryKey
     operations = [
         migrations.CreateModel(
             name='Category',
