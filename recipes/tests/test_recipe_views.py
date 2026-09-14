eed805ec6d84d1767c3c21526dba61c7f99c0c33
Armando Isamu de Almeida Yamada <armandoyamada23@icloud.com>
2026-09-14 15:52:01 -0300

    criação do packge de testes

---
+++ recipes/tests/test_recipe_views.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+from django import views
+
+class RecipeViewsTest(TestCase):
+    def teste_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+    
+    def teste_recipe_category_view_function_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertIs(view.func, views.category)
+        
+    def teste_recipe_detail_view_function_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertIs(view.func, views.recipe)    
